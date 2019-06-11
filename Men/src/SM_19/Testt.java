c3d9bf76fc212177bd929cadb083ab51a9b43b3f
NN98 <jkllhgb@gmail.com>
2019-06-12 08:03:13 +0900

    0610 java

---
+++ Men/src/SM_19/Testt.java
@@ -0,0 +1,30 @@
+package SM_19;
+
+abstract class AAB {
+	void f() {};
+}
+
+public class Testt extends AAB{
+	
+	
+
+
+	public static void main(String[] args) {
+		int i='a';
+//		char b=97;
+		// TODO Auto-generated method stub
+		//AAB a=new AAB();
+		//a.f();
+//		Testt b=new Testt();
+
+		String a="Hello";
+		String b="Hello";
+		String c=new String("Hello");
+		String d=new String("Hello");
+
+		System.out.println(a==b);
+		System.out.println(a==c);
+		
+	}
+
+}
